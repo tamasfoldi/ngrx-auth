eb1b7706f2fd2d531c42b6d1abe3e8663c414955
Tamas Foldi <tamas.foldi@clarmont.hu>
2017-01-20 22:37:01 +1030

    [update] angular-cli

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,34 @@
-// /* tslint:disable:no-unused-variable */
+/* tslint:disable:no-unused-variable */
 
-// import { TestBed, async } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-// describe('App: NgrxAuth', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         AppComponent
-//       ],
-//     });
-//   });
+describe('AppComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    });
+    TestBed.compileComponents();
+  });
 
-//   it('should create the app', async(() => {
-//     let fixture = TestBed.createComponent(AppComponent);
-//     let app = fixture.debugElement.componentInstance;
-//     expect(app).toBeTruthy();
-//   }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 
-//   it(`should have as title 'app works!'`, async(() => {
-//     let fixture = TestBed.createComponent(AppComponent);
-//     let app = fixture.debugElement.componentInstance;
-//     expect(app.title).toEqual('app works!');
-//   }));
+  it(`should have as title 'app works!'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app works!');
+  }));
 
-//   it('should render title in a h1 tag', async(() => {
-//     let fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     let compiled = fixture.debugElement.nativeElement;
-//     expect(compiled.querySelector('h1').textContent).toContain('app works!');
-//   }));
-// });
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  }));
+});
