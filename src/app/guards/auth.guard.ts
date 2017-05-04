b0713672fe678a0e9edb590f339ad3fb718dfc05
Tamas Foldi <tamas.foldi@clarmont.hu>
2017-05-04 23:45:25 +0930

    [refactor] isAuthed

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { State, isAuthed } from '../reducers/auth.reducer';
+import { State, hasNotExpiredAuth } from '../reducers/auth.reducer';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -10,7 +10,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.store.select(isAuthed)
+    return this.store.select(hasNotExpiredAuth)
       .map(isAuthed => !isAuthed)
       .do(notAuthed => !notAuthed ? this.router.navigate(['/secure']) : null);
   }
--- src/app/guards/secure.guard.ts
+++ src/app/guards/secure.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-import { State, isAuthed } from '../reducers/auth.reducer';
+import { State, hasNotExpiredAuth } from '../reducers/auth.reducer';
 
 @Injectable()
 export class SecureGuard implements CanActivate {
@@ -11,7 +11,7 @@ export class SecureGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.store.select(isAuthed)
+    return this.store.select(hasNotExpiredAuth)
       .do(isAuthed => !isAuthed ? this.router.navigate(['/auth']) : null);
   }
 }
