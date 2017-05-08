ecd7f51a8e21def36f2b3beabb9419cf816b4cc7
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-05-08 15:41:29 +0200

    [fix] CI build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -8,6 +8,13 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
+
+  it('should fix te build', () => {
+    expect(true).toBeTruthy();
+  });
 });
